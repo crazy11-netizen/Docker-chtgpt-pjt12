74d41a75d9f9da8f7320e6f498c8bc4583928502
root <root@ip-10-0-15-107.ec2.internal>
2026-07-24 06:59:29 +0000

    some changes

--- database/init.sql
+++ database/init.sql
@@ -3,15 +3,9 @@ CREATE DATABASE IF NOT EXISTS inventory_db;
 USE inventory_db;
 
 CREATE TABLE IF NOT EXISTS products (
-
     id INT AUTO_INCREMENT PRIMARY KEY,
-
     name VARCHAR(100) NOT NULL,
-
     category VARCHAR(100),
-
     price DECIMAL(10,2),
-
     quantity INT
-
 );
